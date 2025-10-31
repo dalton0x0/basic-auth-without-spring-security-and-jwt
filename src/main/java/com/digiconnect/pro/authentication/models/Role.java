865945d31af292051b0d1851c216dc36dfce1ac4
Chéridanh <divintsiela@gmail.com>
2025-10-31 08:11:01 +0100

    fix(roles) : change id generation type to identity

--- src/main/java/com/digiconnect/pro/authentication/models/Role.java
+++ src/main/java/com/digiconnect/pro/authentication/models/Role.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Enumerated(EnumType.STRING)
