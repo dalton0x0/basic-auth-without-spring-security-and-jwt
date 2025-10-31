8d982dae6e05f41d06674c5978384cae486bcded
Chéridanh <divintsiela@gmail.com>
2025-10-31 07:28:23 +0100

    feat(roles) : add role repository

---
+++ src/main/java/com/digiconnect/pro/authentication/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.digiconnect.pro.authentication.repositories;
+
+import com.digiconnect.pro.authentication.enums.RoleType;
+import com.digiconnect.pro.authentication.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(RoleType name);
+}
