dde02cb1e3b2a799e520e8cbb92e3bc099ecf44b
Chéridanh <divintsiela@gmail.com>
2025-10-31 07:16:47 +0100

    chore(exceptions) : add exception handler

---
+++ src/main/java/com/digiconnect/pro/authentication/exceptions/NoSuchRoleException.java
@@ -0,0 +1,7 @@
+package com.digiconnect.pro.authentication.exceptions;
+
+public class NoSuchRoleException extends RuntimeException{
+    public NoSuchRoleException(String message) {
+        super(message);
+    }
+}
