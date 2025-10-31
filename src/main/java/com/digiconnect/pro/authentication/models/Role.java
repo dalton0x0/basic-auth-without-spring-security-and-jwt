0a4e35f62ed8eaf398c574e9e9949970272b51ce
Chéridanh <divintsiela@gmail.com>
2025-10-31 07:13:01 +0100

    feat(roles) : add role entity

---
+++ src/main/java/com/digiconnect/pro/authentication/models/Role.java
@@ -0,0 +1,24 @@
+package com.digiconnect.pro.authentication.models;
+
+import com.digiconnect.pro.authentication.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType name;
+}
