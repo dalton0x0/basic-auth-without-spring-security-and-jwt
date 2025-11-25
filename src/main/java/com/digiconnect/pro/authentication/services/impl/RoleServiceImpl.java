d4ef4cdd649f42a930aa7c8e4a2fee3310406092
Chéridanh <divintsiela@gmail.com>
2025-11-25 16:47:27 +0100

    fix(roles) : change integer to long on role implementation

--- src/main/java/com/digiconnect/pro/authentication/services/impl/RoleServiceImpl.java
+++ src/main/java/com/digiconnect/pro/authentication/services/impl/RoleServiceImpl.java
@@ -4,11 +4,14 @@ import com.digiconnect.pro.authentication.dtos.RoleRequestDto;
 import com.digiconnect.pro.authentication.dtos.RoleResponseDto;
 import com.digiconnect.pro.authentication.enums.RoleType;
 import com.digiconnect.pro.authentication.exceptions.BadRequestException;
+import com.digiconnect.pro.authentication.exceptions.InvalidTokenException;
 import com.digiconnect.pro.authentication.exceptions.NoSuchRoleException;
 import com.digiconnect.pro.authentication.exceptions.RoleNameAlreadyInUseException;
 import com.digiconnect.pro.authentication.mappers.RoleMapper;
 import com.digiconnect.pro.authentication.models.Role;
+import com.digiconnect.pro.authentication.models.User;
 import com.digiconnect.pro.authentication.repositories.RoleRepository;
+import com.digiconnect.pro.authentication.repositories.UserRepository;
 import com.digiconnect.pro.authentication.services.RoleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,50 +24,83 @@ import java.util.List;
 @Slf4j
 public class RoleServiceImpl implements RoleService {
 
+    private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final RoleMapper roleMapper;
 
     @Override
-    public List<RoleResponseDto> getAllRoles() {
+    public List<RoleResponseDto> getAllRoles(String sessionToken) {
+
         log.debug("Getting all roles");
+
+        verifyRoleUserBySessionToken(sessionToken);
+
         List<RoleResponseDto> roles = roleRepository.findAll()
                 .stream()
                 .map(roleMapper::convertEntityToDto)
                 .toList();
         log.info("Total roles get : {}, roles found: {}", roles.size(),  roles);
+
         return roles;
     }
 
     @Override
-    public RoleResponseDto getRoleById(Integer id) {
+    public RoleResponseDto getRoleById(Long id, String sessionToken) {
+
         log.debug("Getting role with id : {}", id);
+
+        verifyRoleUserBySessionToken(sessionToken);
+
         Role existingRole = roleRepository.findById(id)
                 .orElseThrow(() -> new NoSuchRoleException("Role not found"));
         log.info("Role get : {}", existingRole.getName());
+
         return roleMapper.convertEntityToDto(existingRole);
     }
 
     @Override
-    public RoleResponseDto createRole(RoleRequestDto roleRequestDto) {
+    public RoleResponseDto createRole(RoleRequestDto roleRequestDto, String sessionToken) {
+
         log.info("Creating role : {}", roleRequestDto);
+
+        verifyRoleUserBySessionToken(sessionToken);
+
         roleRepository.findByName(roleRequestDto.getName()).ifPresent(existingRole -> {
             throw new RoleNameAlreadyInUseException("Role already exists");
         });
         Role role = roleMapper.convertDtoToEntity(roleRequestDto);
         Role savedRole = roleRepository.save(role);
+
         log.info("Saved role : {}", savedRole);
+
         return roleMapper.convertEntityToDto(savedRole);
     }
 
     @Override
-    public void deleteRoleById(Integer id) {
+    public void deleteRoleById(Long id, String sessionToken) {
+
         log.info("Deleting role with id : {}", id);
+
+        verifyRoleUserBySessionToken(sessionToken);
+
         Role role = roleRepository.findById(id)
                 .orElseThrow(() -> new NoSuchRoleException("Role not found"));
         if (role.getName().equals(RoleType.ADMIN)) {
             throw new BadRequestException("Cannot delete admin role");
         }
+
         roleRepository.deleteById(role.getId());
+
         log.info("Deleted role : {}", role.getName());
+    }
+
+    private void verifyRoleUserBySessionToken(String sessionToken) {
+        User user = userRepository.findBySessionToken(sessionToken)
+                .orElseThrow(() -> new InvalidTokenException("Invalid token session"));
+
+        if (!user.isAdmin()) {
+            log.error("User : {}, has not an admin role to do this action", user.getFullName());
+            throw new InvalidTokenException("Only admins can do this");
+        }
     }
 }
--- src/main/java/com/digiconnect/pro/authentication/web/RoleController.java
+++ src/main/java/com/digiconnect/pro/authentication/web/RoleController.java
@@ -19,26 +19,32 @@ public class RoleController {
     private final RoleService roleService;
 
     @GetMapping("")
-    public ResponseEntity<List<RoleResponseDto>> getAllRoles() {
-        List<RoleResponseDto> roles = roleService.getAllRoles();
+    public ResponseEntity<List<RoleResponseDto>> getAllRoles(@RequestHeader("X-Session-Token") String sessionToken) {
+        List<RoleResponseDto> roles = roleService.getAllRoles(sessionToken);
         return roles.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(roles);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<RoleResponseDto> getRoleById(@PathVariable Integer id) {
-        RoleResponseDto role = roleService.getRoleById(id);
+    public ResponseEntity<RoleResponseDto> getRoleById(
+            @RequestHeader("X-Session-Token") String sessionToken,
+            @PathVariable Long id) {
+        RoleResponseDto role = roleService.getRoleById(id, sessionToken);
         return ResponseEntity.ok(role);
     }
 
     @PostMapping("")
-    public ResponseEntity<RoleResponseDto> createRole(@RequestBody @Valid RoleRequestDto roleRequestDto) {
-        RoleResponseDto role = roleService.createRole(roleRequestDto);
+    public ResponseEntity<RoleResponseDto> createRole(
+            @RequestHeader("X-Session-Token") String sessionToken,
+            @RequestBody @Valid RoleRequestDto roleRequestDto) {
+        RoleResponseDto role = roleService.createRole(roleRequestDto, sessionToken);
         return ResponseEntity.status(HttpStatus.CREATED).body(role);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<RoleResponseDto> deleteRoleById(@PathVariable Integer id) {
-        roleService.deleteRoleById(id);
+    public ResponseEntity<RoleResponseDto> deleteRoleById(
+            @RequestHeader("X-Session-Token") String sessionToken,
+            @PathVariable Long id) {
+        roleService.deleteRoleById(id, sessionToken);
         return ResponseEntity.noContent().build();
     }
 }
